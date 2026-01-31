03326946d642b818a13c0c247bf8515dcafbc9d3
Eugenio Milazzo <ioiugi04@gmail.com>
2026-01-31 23:14:24 +0100

    Feat: Created Obstacle Data scriptable object

---
+++ My project/Assets/Scripts/Obstacles/Obstacle_Data.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Obstacle_", menuName = "Scriptable Objects/Obstacle_Data")]
+public class Obstacle_Data : ScriptableObject
+{
+    public string ObstacleTag;
+}
