f18d1a0503da2a5de6771ead080500cc79818757
Eugenio Milazzo <ioiugi04@gmail.com>
2026-01-31 23:13:24 +0100

    Feat: Created Sound Manager class

---
+++ My project/Assets/Scripts/sfx/SoundManager.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] private List<string> _clipTags;
+    [SerializeField] private List<AudioClip> _clips;
+
+    public static SoundManager Instance;
+    
+    private Dictionary<string, AudioClip> _clipMap;
+    private AudioSource _audioSource;
+
+    private void Awake()
+    {
+        _clipMap = new Dictionary<string, AudioClip>();
+        
+        for (int i = 0; i < _clipTags.Count; i++)
+        {
+            _clipMap.Add(_clipTags[i], _clips[i]);
+            
+            Debug.Log(_clipMap[_clipTags[i]]);
+        }
+
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(Instance);
+        }
+        else
+            Destroy(gameObject);
+    }
+    
+    public void PlayCorrespondingSound(string tag)
+    {
+        _clipMap.TryGetValue(tag, out AudioClip clip);
+        
+        _audioSource.PlayOneShot(clip);
+    }
+}
