c995a4ac51ad98d0f653286deb9f23bd21b09440
Eugenio Milazzo <ioiugi04@gmail.com>
2026-02-01 16:24:20 +0100

    Feat: one final small fix

--- My project/Assets/Scripts/sfx/IntroPlayer.cs
+++ My project/Assets/Scripts/sfx/IntroPlayer.cs
@@ -1,18 +1,30 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class IntroPlayer : MonoBehaviour
 {
     [SerializeField] private AudioSource _audioSource;
+
+    private bool _startScene;
     
     private void Awake()
     {
         StartCoroutine(StartLevel());
     }
 
+    private void Update()
+    {
+        if (_startScene)
+        {
+            SceneManager.Instance.LoadScene(1);
+        }
+    }
+
     private IEnumerator StartLevel()
     {
-        yield return new WaitForSeconds(_audioSource.clip.length);
+        yield return new WaitForSeconds(44);
 
-        SceneManager.Instance.LoadScene(1);
+        _startScene = true;
     }
 }
