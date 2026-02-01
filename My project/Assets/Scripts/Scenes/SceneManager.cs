24115b06f3d037e0366e61f8d0a21f92bdb88b34
Eugenio Milazzo <ioiugi04@gmail.com>
2026-02-01 12:51:46 +0100

    Feat: Created scene manager class

---
+++ My project/Assets/Scripts/Scenes/SceneManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour
+{
+    public static SceneManager Instance;
+    [SerializeField] private Scene[] _scene;
+    
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(Instance);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void LoadScene(int index)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(index);
+    }
+}
